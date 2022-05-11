c70871bd860d8846c179f0a66803417288ea96d0
xXForeXx <daniel199938@gmail.com>
2022-05-10 22:26:35 -0400

    primer commit despues de tanto trabajo

---
+++ js.js
@@ -0,0 +1,20 @@
+function desplieguemenu() {
+    var x = document.getElementById('desplegable_menu');
+    if (x.style.display === 'none') {
+       x.style.display = 'flex'; 
+    }
+    else {
+      x.style.display = 'none';
+    }
+  };
+
+function despliegueContact(){
+    var y = document.getElementById('desplegable_Contact');
+    if (y.style.display === 'none'){
+        y.style.display = 'flex';
+    }
+    else {
+        y.style.display = 'none';
+    }
+    
+};
